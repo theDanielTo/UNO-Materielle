c4654dffcf46a233d32c64215b6853bc6a01f170
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-08-24 16:21:44 -0700

    Created temp About page, and working on the react-router.

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
+import About from './pages/about';
 
-export default class App extends React.Component {
-  render() {
-    return <Home />;
-  }
+// Route renders component based on URL.
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-export default function Home(props) {
+function Home() {
   return (
-    <>
-    </>
+    <div>
+      <h1>Home Page</h1>
+    </div>
   );
 }
+
+export default Home;
