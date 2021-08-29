0f2d7cbab91a3d58a9fa0d226cb79785858f2772
Brian Bozigian <bbozigian@outlook.com>
2021-08-29 21:48:23 +0000

    edited about us page

--- client/pages/about-us.jsx
+++ client/pages/about-us.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Grid } from '@material-ui/core';
+import { Paper, Grid, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: '#151224',
     color: '#DDE0EF',
     height: '93.6vh',
+    minHeight: '95.6vh',
     maxWidth: '1200px',
-    paddingBottom: '1em',
+    // paddingBottom: '1em',
     fontSize: '1.5rem'
   },
   about: {
@@ -63,6 +64,10 @@ const useStyles = makeStyles(theme => ({
     width: 'auto',
     marginLeft: '-100px',
     marginTop: '-5px'
+  },
+  title: {
+    marginLeft: '10px',
+    marginTop: '18px'
   }
 }));
 
@@ -71,7 +76,8 @@ export default function AboutUs() {
 
   return (
     <div className={classes.root}>
-      <h1 className="poppins">About Us</h1>
+      <Typography variant="h3" className={classes.title}> About Us</Typography>
+      {/* <h1 className="poppins" className>About Us</h1> */}
       <hr />
       <Grid container spacing={6} direction="row" justifyContent="space-around">
         <Grid item md={4}>
@@ -79,8 +85,10 @@ export default function AboutUs() {
             <div className={classes.trial}>
               <img className={classes.brian}src="/images/devs/brianbozigian.png" alt="Brian's Picture"></img>
             </div>
-            <p className={classes.header}>Brian Bozigan</p>
-            <p className={classes.infoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</p>
+            <Typography variant="h4" className={classes.header}>Brian Bozigian</Typography>
+            <Typography variant="h6" className={classes.infoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</Typography>
+            {/* <p className={classes.header}>Brian Bozigan</p>
+            <p className={classes.infoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</p> */}
             <Paper className={classes.link}>
               <a href="https://github.com/BrianBozi" target="_blank" rel="noreferrer"><span className={classes.logo}><i className="fab fa-github-square fa-2x"></i></span></a>
               <a href="https://www.linkedin.com/in/brian-bozigian/" target="_blank" rel="noreferrer"><span className={classes.logo}><i className="fab fa-linkedin fa-2x"></i></span></a>
@@ -92,8 +100,10 @@ export default function AboutUs() {
             <div className={classes.trial}>
               <img className={classes.daniel} src="/images/devs/danielto.png" alt="Daniel's Picture"></img>
             </div>
-            <p className={classes.header}>Daniel To</p>
-            <p className={classes.infoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</p>
+            <Typography variant="h4" className={classes.header}> Daniel To</Typography>
+            <Typography variant="h6" className={classes.infoText}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</Typography>
+            {/* <p className={classes.header}>Daniel To</p>
+            <p className={classes.infoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</p> */}
             <Paper className={classes.link}>
               <a href="https://github.com/theDanielTo" target="_blank" rel="noreferrer"><span className={classes.logo}><i className="fab fa-github-square fa-2x"></i></span></a>
               <a href="https://www.linkedin.com/in/to-daniel/" target="_blank" rel="noreferrer"><span className={classes.logo}><i className="fab fa-linkedin fa-2x"></i></span></a>
@@ -105,8 +115,10 @@ export default function AboutUs() {
             <div className={classes.trial}>
               <img className={classes.michael} src="/images/devs/michaelaguilar.png" alt="Michael's Picture"></img>
             </div>
-            <p className={classes.header}>Michael Aguilar</p>
-            <p className={classes.infoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</p>
+            <Typography variant="h4" className={classes.header}>Michael Aguilar</Typography>
+            <Typography variant="h6" className={classes.infoText}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</Typography>
+            {/* <p className={classes.header}>Michael Aguilar</p>
+            <p className={classes.infoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore quod ipsa, velit eveniet non nulla! Pariatur vero non provident temporibus sapiente a consectetur ad?</p> */}
             <Paper className={classes.link}>
               <a href="https://github.com/Michael-M-Aguilar" target="_blank" rel="noreferrer"><span className={classes.logo}><i className="fab fa-github-square fa-2x"></i></span></a>
               <a href="https://www.linkedin.com/in/michael-m-aguilar/" target="_blank" rel="noreferrer"><span className={classes.logo}><i className="fab fa-linkedin fa-2x"></i></span></a>
