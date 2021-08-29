22a7edec1b0ab6944d623aced52def1a543d7a7a
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-08-29 16:00:53 -0700

    Created the component for Chat Side Nav Bar. Just have to add socket, and have the icon flex end or be at the end.

--- client/components/chat-nav-bar.jsx
+++ client/components/chat-nav-bar.jsx
@@ -5,9 +5,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Drawer, Hidden, IconButton } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import { Divider, Drawer, Hidden, IconButton, ListItem } from '@material-ui/core';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import { List } from '@material-ui/icons';
+import FormDialog from './name-dialogs';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const drawerWidth = 200;
+const drawerWidth = 400;
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -43,4 +49,75 @@ export default function ChatNav(props) {
 
   const container =
     window !== undefined ? () => window().document.body : undefined;
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider light={true} />
+      <List>
+        <ListItem alignItems="center">
+          <ListItemIcon className={classes.listItem}>
+            <Avatar>M</Avatar>
+          </ListItemIcon>
+          <ListItemText primary="Uno!" className={classes.listItem} />
+        </ListItem>
+      </List>
+      <Divider light={true} />
+      <List>
+      {/* Insert Socket */}
+      </List>
+      <List>
+        <FormDialog />
+      </List>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle} >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <nav aria-label="sidenav">
+        <Hidden lgUp implementation="css">
+          <Drawer
+            anchor='right'
+            container={container}
+            className={classes.drawer}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden lgDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+    </div>
+  );
+
 }
