5e99a154c4d1819d9d828d3b43f7d45b1e83137a
theDanielTo <dto1989@gmail.com>
2021-08-30 16:47:54 -0700

    changed file names

--- client/app.jsx
+++ client/app.jsx
@@ -3,10 +3,10 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import SideNav from './components/sidenav';
 import Home from './pages/home';
-import HowToPlay from './pages/howtoplay';
-import AboutUs from './pages/aboutus';
+import HowToPlay from './pages/how-to-play';
+import AboutUs from './pages/about-us';
 import Lobby from './pages/lobby';
-import GameBoard from './components/gameBoard';
+import GameBoard from './components/game-board';
 
 const useStyles = makeStyles(theme => ({
   root: {
